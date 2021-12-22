ce4f6140e3b6af02326f818f1ac6d1dd6656b42f
naufaladrna08 <naufaladrn90@gmail.com>
2021-12-22 18:46:31 +0700

    added header

--- Views/main.php
+++ Views/main.php
@@ -11,7 +11,16 @@
   <title> <?= $this->title ?> </title>
 </head>
 <body>
-  <?php require_once($page_path); ?>
+  <?php 
+  
+  if (!isset($_SESSION['LOGGEDUSER'])) {
+    require_once('Views/navbar-notlogged.php');
+  } else {
+    require_once('Views/navbar-logged.php');
+  }
+
+  require_once($page_path);
+  ?>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
