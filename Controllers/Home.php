5531ea6e71a6d7b0a89006aafc754e6a638c33e0
naufaladrna08 <naufaladrn90@gmail.com>
2021-12-23 08:46:22 +0700

    add home page

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -1,6 +1,17 @@
 <?php
 
 class Home extends Controller {
+  public static function Response($code, $status, $msg, $dt = []) {
+    $data = [
+      'code'    => $code,
+      'status'  => $status,
+      'message' => $msg,
+      'data'    => $dt
+    ];
+
+    return $data;
+  }
+
   public function __default() {
     global $db;
 
@@ -11,6 +22,74 @@ class Home extends Controller {
     $this->render('index', [
       'user' => $_SESSION['LOGGEDUSER']
     ]);
+  }
+
+  public function getarticle() {
+    global $db;
+    $data = [];
+
+    try {
+      $stmt = $db->prepare("
+        SELECT
+          aa.id,
+          aa.uid,
+          aa.title,
+          aa.description,
+          aa.category,
+          bb.username
+        FROM
+          posts aa
+        LEFT JOIN users bb ON aa.uid = bb.user_id
+      ");
+      $stmt->execute();
+
+      $posts = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+      if ($stmt->rowCount() > 0) {
+        $_data = '';
+
+        foreach ($posts as $v) {
+          $c = str_replace('/', ',', $v['category']);
+          $_cats = '';
+
+          $cat = $db->prepare('SELECT category FROM categories WHERE id IN ('. $c .')');
+          $cat->execute();
+          $cats = $cat->fetchAll(\PDO::FETCH_ASSOC);
+
+          foreach ($cats as $_c) {
+            $_cats .= '<span class="badge rounded-pill bg-primary mr-2">'. $_c['category'] .'</span>';
+          }
+
+          $_data .= '
+            <div class="row mb-4">
+              <div class="col-2 col-sm-2 col-md-1">
+                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" width="48" height="48" class="avatar">
+              </div>
+              <div class="col-10 col-sm-10 col-md-11">
+                <h5> '. $v['title'] .' </h5>
+                <div class="mb-3"> '. $_cats .'  </div>
+                <p class="lead">
+                  '. $v['description'] .'
+                </p>
+
+                <a href="'. BASE_URL .'index.php?p=profile/'. $v['username'] .'">
+                  <i class="fas fa-user mr-4"> </i> '. $v['username'] .'
+                </a>
+              </div>
+            </div>
+          ';
+        }
+
+        $data = self::Response(200, 'Success', 'Data ditemukan', $_data);
+      } else {
+        $data = self::Response(404, 'Failed', 'Data tidak ditemukan', null);
+      }
+    } catch (PDPException $e) {
+      $data = self::Response(500, 'Failed', 'Internal Server Error', $e->getMessage());
+    }
+
+    header('Content-Type: application/json; charset=utf-8');
+    return $this->json($data);
   }
 
   public function logout() {
---
+++ Views/Home/index.php
@@ -0,0 +1,41 @@
+<div id="root">
+  <div class="container my-4">
+    <div class="col-sm-12 col-md-8">
+      <h4> Discover new Articles </h4>
+
+      <div id="post"></div>
+    </div>
+    <div class="col-sm-12 col-md-4">
+
+    </div>
+  </div>
+</div>
+
+<style>
+  #root {
+    padding: 4em 0;
+  }
+
+  body {
+    font-family: 'Raleway', sans-serif;
+  }
+</style>
+
+<script>
+  $(document).ready(() => {
+    $.ajax({
+      url: "<?= BASE_URL ?>index.php?p=home/getarticle",
+      dataType: "JSON",
+      beforeSend: () => {
+        $('#post').html('Loading...')
+      },
+      success: (resp) => {
+        if (resp.code != 200) {
+          $('#post').html('Oops, artikelnya masih kosong nih. Ayo bikin artikel baru!.')
+        } else {
+          $('#post').html(resp.data)
+        }
+      }
+    })
+  })
+</script>
