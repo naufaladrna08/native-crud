d1a08b56b54e62591de4cc9f2349796f076278f6
naufaladrna08 <naufaladrn90@gmail.com>
2021-12-24 15:42:45 +0700

    added menu for update and delete

--- Views/Article/index.php
+++ Views/Article/index.php
@@ -1,9 +1,28 @@
 <div id="root">
   <div class="container">
-    <h1> <?= $data['title'] ?> </h1>
-    <small> by <?= $data['username'] ?> </small>
+    <div class="row">
+      <div class="col-md-9">
+        <h1> <?= $data['title'] ?> </h1>
+        <small> by <?= $data['username'] ?> on <?= $data['created_at'] ?> </small>
 
-    <p class="lead mt-4"> <?= $data['description'] ?> </p>
+        <?php 
+        if ($data['username'] == $_SESSION['LOGGEDUSER'][0]['username']) {
+        ?>
+        <nav class="mt-4">
+          <a href="<?= BASE_URL ?>index.php?p=article/update/<?= $data['id'] ?>" class="btn btn-primary btn-sm"> Update </a>
+          <a href="<?= BASE_URL ?>index.php?p=article/adelete/<?= $data['id'] ?>" class="btn btn-danger btn-sm"> Delete </a>
+        </nav>
+        <?php 
+        }
+        ?>
+
+        <p class="lead mt-2"> <?= $data['description'] ?> </p>
+      </div>
+      <div class="col-md-3">
+        <div id="ad-banner"></div>
+        <div id="ad-banner" class="mt-2"></div>
+      </div>
+    </div>
   </div>
 </div>
 
@@ -12,4 +31,9 @@
     padding: 6em 0;
   }
 
+  #ad-banner {
+    width: 100%;
+    height: 400px;
+    background: #ebebeb;
+  }
 </style>
